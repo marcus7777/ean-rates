9e7a139232cde8627d4ae1db4a1b8b900310f455
Marcus Hitchins <marcus@hhost.me>
2016-01-04 17:52:05 +0000

    Update config.php

--- server/config.php
+++ server/config.php
@@ -1,5 +1,3 @@
+<?php
 $apiKey = '15qo09ab6dge7cdp04nu3kcav5';
 $secret = 'cvh43jpjctvt2';
-$timestamp = gmdate('U'); // 1427233130 (Tue, 24 Mar 2015 21:38:50 +0000)
-$sig = md5($apiKey . $secret . $timestamp);
-$add_to_url = "&apiKey=" . $apiKey . "&sig=" . $sig;
