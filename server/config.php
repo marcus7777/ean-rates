fb2fca263b676af2229906faca307cbd51897ac6
Marcus Hitchins <marcus@hhost.me>
2016-01-04 17:05:23 +0000

    Create config.php

---
+++ server/config.php
@@ -0,0 +1,4 @@
+$apiKey = '15qo09ab6dge7cdp04nu3kcav5';
+$secret = 'cvh43jpjctvt2';
+$timestamp = gmdate('U'); // 1427233130 (Tue, 24 Mar 2015 21:38:50 +0000)
+$sig = md5($apiKey . $secret . $timestamp);
